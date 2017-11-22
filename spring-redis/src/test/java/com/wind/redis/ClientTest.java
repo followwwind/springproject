47a3caa1140828b0ae76488bc4a7dfb309043f1e
followwwind <18771933975@163.com>
2017-11-22 17:07:00 +0800

    重构spring-redis模块

---
+++ spring-redis/src/test/java/com/wind/redis/ClientTest.java
@@ -0,0 +1,19 @@
+package com.wind.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Redis client测试
+ * @author wind
+ */
+public class ClientTest {
+
+    public static void connect(){
+        Jedis jedis = new Jedis("127.0.0.1", 6479);
+        System.out.println(jedis.ping());
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}
