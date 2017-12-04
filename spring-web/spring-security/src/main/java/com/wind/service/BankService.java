02ce46d380ca8baca89ca8a0d916e55fb8d1b0e1
followwwind <18771933975@163.com>
2017-12-04 14:21:00 +0800

    完善spring-security模块结构

---
+++ spring-web/spring-security/src/main/java/com/wind/service/BankService.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wind.service;
+
+import com.wind.entity.Account;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * @author wind
+ */
+public interface BankService {
+
+	/**
+	 * 获取账户
+	 * @param id
+	 * @return
+	 */
+	Account readAccount(Long id);
+
+	/**
+	 * 查询所有账户
+	 * @return
+	 */
+	Account[] findAccounts();
+
+	/**
+	 * 新增或者修改账户信息
+	 * @param account
+	 * @param amount
+	 * @return
+	 */
+	@PreAuthorize("hasRole('supervisor') or "
+			+ "hasRole('teller') and (#account.balance + #amount >= -#account.overdraft)")
+	Account post(Account account, double amount);
+}
